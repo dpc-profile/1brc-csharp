78c10da09bf0f85d7ca6424ba22354aa3b3ce0f0
Daniel Castro <daniel.pcastro11@gmail.com>
2024-07-18 20:29:10 -0300

    FASTER!!!

--- publish.bat
+++ publish.bat
@@ -1,3 +1,1 @@
-rem dotnet publish -c Release --no-restore .\1brc\1brc.csproj
-
-dotnet publish -c Release --self-contained .\1brc\1brc.csproj -f net6.0 -r win-x64 -o .\1brc\bin\Release\win-x64\
+dotnet publish -c Release .\1brc\1brc.csproj -f net6.0 -r win-x64 -o .\1brc\bin\Release\win-x64\
